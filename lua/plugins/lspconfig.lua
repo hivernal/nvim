1d0306556f6f2f64c59e6fa89c5d6f6d98c1ad07
hivernal <geryaevna@mail.ru>
2023-08-21 15:25:06 +0800

    add nvim-formatter and nvim-lint plugins

--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -1,48 +1,89 @@
 return {
-  "neovim/nvim-lspconfig",
-  dependencies = {
-    { "williamboman/mason.nvim", opts = {} },
-  },
-  event = { "BufReadPre", "BufNewFile" },
-  config = function()
-    local lspconfig = require("lspconfig")
-    local capabilities = require("cmp_nvim_lsp").default_capabilities()
+	"neovim/nvim-lspconfig",
+	dependencies = {
+		{ "williamboman/mason.nvim", opts = {} },
 
-    lspconfig.clangd.setup({ capabilities = capabilities })
-    lspconfig.rust_analyzer.setup({ capabilities = capabilities })
-    lspconfig.lua_ls.setup({ capabilities = capabilities })
+		{
+			"mfussenegger/nvim-lint",
+			config = function()
+				require("lint").linters_by_ft = {
+					cpp = { "cpplint" },
+				}
+				vim.api.nvim_create_autocmd({ "BufWritePost" }, {
+					callback = function()
+						require("lint").try_lint()
+					end,
+				})
+			end,
+		},
 
-    local signs = { Error = " ", Warn = " ", Hint = " ", Info = " " }
-    for type, icon in pairs(signs) do
-      local hl = "DiagnosticSign" .. type
-      vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = hl })
-    end
+		{
+			"mhartington/formatter.nvim",
+      keys = {
+        { "<leader>lf", "<cmd>Format<cr>", desc = "Formatting" },
+      },
+			config = function()
+				require("formatter").setup({
+					filetype = {
+						lua = {
+							require("formatter.filetypes.lua").stylua,
+						},
+						cpp = {
+							require("formatter.filetypes.cpp").clangformat,
+						},
+            python = {
+              require("formater.filetypes.python").autopep8,
+            },
+						["*"] = {
+							require("formatter.filetypes.any").remove_trailing_whitespace,
+						},
+					},
+				})
+			end,
+		},
+	},
+	event = { "BufReadPre", "BufNewFile" },
+	config = function()
+		local lspconfig = require("lspconfig")
+		local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
-    vim.api.nvim_create_autocmd("LspAttach", {
-      group = vim.api.nvim_create_augroup("UserLspConfig", {}),
-      callback = function(ev)
-        -- Enable completion triggered by <c-x><c-o>
-        vim.bo[ev.buf].omnifunc = "v:lua.vim.lsp.omnifunc"
-        -- Buffer local mappings.
-        -- See `:help vim.lsp.*` for documentation on any of the below functions
-        vim.keymap.set("n", "<leader>lD", vim.lsp.buf.declaration, { buffer = ev.buf, desc = "Declaration" })
-        vim.keymap.set("n", "<leader>ld", vim.lsp.buf.definition, { buffer = ev.buf, desc = "Definition" })
-        vim.keymap.set("n", "K", vim.lsp.buf.hover, { buffer = ev.buf })
-        vim.keymap.set("n", "<leader>li", vim.lsp.buf.implementation, { buffer = ev.buf, desc = "Implementation" })
-        vim.keymap.set("n", "<C-k>", vim.lsp.buf.signature_help, { buffer = ev.buf })
-        --[[ vim.keymap.set("n", "<leader>wa", vim.lsp.buf.add_workspace_folder,
-          { buffer = ev.buf, desc = "Add workspace folder" })
-        vim.keymap.set("n", "<leader>wr", vim.lsp.buf.remove_workspace_folder,
-          { buffer = ev.buf, desc = "Remove workspace folder" })
-        vim.keymap.set("n", "<leader>wl", function()
-          print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
-        end, { buffer = ev.buf, desc = "List workspace folder" }) ]]
-        vim.keymap.set("n", "<leader>ln", vim.lsp.buf.rename, { buffer = ev.buf, desc = "Rename" })
-        vim.keymap.set({ "n", "v" }, "<leader>la", vim.lsp.buf.code_action, { buffer = ev.buf, desc = "Code action" })
-        vim.keymap.set("n", "<leader>lf", function()
-          vim.lsp.buf.format { async = true }
-        end, { buffer = ev.buf, desc = "Formatting" })
-      end,
-    })
-  end
+		lspconfig.clangd.setup({ capabilities = capabilities })
+		lspconfig.lua_ls.setup({ capabilities = capabilities })
+		lspconfig.pyright.setup({ capabilities = capabilities })
+
+		local signs = { Error = " ", Warn = " ", Hint = " ", Info = " " }
+		for type, icon in pairs(signs) do
+			local hl = "DiagnosticSign" .. type
+			vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = hl })
+		end
+		vim.keymap.set("n", "<leader>lk", vim.diagnostic.goto_prev, { desc = "Previous diagnostic" })
+		vim.keymap.set("n", "<leader>lj", vim.diagnostic.goto_next, { desc = "Next diagnostic" })
+		vim.keymap.set("n", "<leader>lc", vim.diagnostic.open_float, { desc = "Current diagnostic" })
+		vim.api.nvim_create_autocmd("LspAttach", {
+			group = vim.api.nvim_create_augroup("UserLspConfig", {}),
+			callback = function(ev)
+				vim.bo[ev.buf].omnifunc = "v:lua.vim.lsp.omnifunc"
+				vim.keymap.set("n", "<leader>lD", vim.lsp.buf.declaration, { buffer = ev.buf, desc = "Declaration" })
+				vim.keymap.set("n", "<leader>ld", vim.lsp.buf.definition, { buffer = ev.buf, desc = "Definition" })
+				vim.keymap.set("n", "K", vim.lsp.buf.hover, { buffer = ev.buf })
+				vim.keymap.set(
+					"n",
+					"<leader>li",
+					vim.lsp.buf.implementation,
+					{ buffer = ev.buf, desc = "Implementation" }
+				)
+				vim.keymap.set("n", "<C-k>", vim.lsp.buf.signature_help, { buffer = ev.buf })
+				vim.keymap.set("n", "<leader>ln", vim.lsp.buf.rename, { buffer = ev.buf, desc = "Rename" })
+				vim.keymap.set(
+					{ "n", "v" },
+					"<leader>la",
+					vim.lsp.buf.code_action,
+					{ buffer = ev.buf, desc = "Code action" }
+				)
+				-- vim.keymap.set("n", "<leader>lf", function()
+				--   vim.lsp.buf.format { async = true }
+				-- end, { buffer = ev.buf, desc = "Formatting" })
+			end,
+		})
+	end,
 }
